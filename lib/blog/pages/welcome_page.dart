c25d10eee9643157f5b0991a4af8842d7bf7d175
Kingtous <kingtous@qq.com>
2020-11-12 20:15:27 +0800

    优化移动端网页效果

--- lib/blog/pages/welcome_page.dart
+++ lib/blog/pages/welcome_page.dart
@@ -6,13 +6,12 @@
  * @Description: 带搜索条的欢迎页
  */
 import 'package:flare_flutter/flare_actor.dart';
-import 'package:flare_flutter/flare_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:kingtous_blog/blog/detail/providers/blog_detail_provider.dart';
 import 'package:overlay_support/overlay_support.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 import 'package:theme_provider/theme_provider.dart';
 
@@ -41,51 +40,61 @@ class _WelcomePageState extends State<WelcomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Positioned.fill(
-          child: FlareActor(
-            "assets/flrs/welcome3.flr",
-            animation: ThemeProvider.themeOf(context).id == 'light'? "welcome_dark":'welcome',
-            fit: BoxFit.cover,
-            callback: (s) {
-              Get.log("animation ended");
-            },
+    return Container(
+      decoration: BoxDecoration(
+          color: ThemeProvider.themeOf(context).id == 'light'
+              ? Color(0xFF217FCC)
+              : Colors.black),
+      child: Stack(
+        children: [
+          SizedBox(
+            height: double.infinity,
+            width: double.infinity,
+            child: FlareActor(
+              "assets/flrs/welcome.flr",
+              animation: ThemeProvider.themeOf(context).id == 'light'
+                  ? "welcome"
+                  : 'welcome_dark',
+              fit: BoxFit.fitWidth,
+              callback: (s) {
+                Get.log("animation ended");
+              },
+            ),
           ),
-        ),
-        Container(
-          alignment: Alignment.center,
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(300.w),
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                              color: Colors.white),
-                          borderRadius: BorderRadius.all(Radius.circular(40))),
-                      child: TextField(
-                      
-                        style: TextStyle(color: Colors.white),
-                        decoration: InputDecoration(
-                            border: InputBorder.none,
-                            contentPadding: EdgeInsets.only(top: 22.h),
-                            hintText: "搜索内容",
-                            hintStyle: TextStyle(color: Colors.white),
-                            prefixIcon: Icon(Icons.search,color: Colors.white)),
-                        controller: _searchTextController,
-                        onSubmitted: (s) => _search(s),
+          Container(
+            alignment: Alignment.center,
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        margin: EdgeInsets.all(300.w),
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.white),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(40))),
+                        child: TextField(
+                          style: TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              contentPadding: EdgeInsets.only(top: 22.h),
+                              hintText: "搜索内容",
+                              hintStyle: TextStyle(color: Colors.white),
+                              prefixIcon:
+                                  Icon(Icons.search, color: Colors.white)),
+                          controller: _searchTextController,
+                          onSubmitted: (s) => _search(s),
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
